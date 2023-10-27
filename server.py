ef0b38d297b2449f57a380be9fbb49aae57ac249
Emily Law <emilyelhy@yahoo.com>
2023-10-27 22:30:04 +0800

    change password func done

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ MONGODB_COLLECTION = os.environ.get("MONGODB_COLLECTION")
 print("[Flask server.py] Flask server connected to " + str(MONGODB_URI))
 
 # return true if the distance between 2 points is less than 100m
-def validateDistance(lat1, lon1, lat2, lon2):
+def validate_distance(lat1, lon1, lat2, lon2):
     # Radius of the Earth in km
     radius = 6371
     # Convert latitude and longitude to radians
@@ -38,7 +38,8 @@ def test():
     return {"res": "success"}
 
 # handling login requests from react
-# Input: object of groupNo and password
+# param: object of groupNo and password
+# return: true on successful login and false for failed login
 @app.route('/login', methods=['POST'])
 def login():
     print("[Flask server.py] POST path /login")
@@ -53,7 +54,8 @@ def login():
     return {"res": "fail"}
 
 # handling create user requests from REST API directly
-# list named as userList containing all user's groupNo and password as object
+# param: list named as userList containing all user's groupNo and password as object
+# return: true on successful user creation and false on failed creation
 @app.route('/createuser', methods=['POST'])
 def create_user():
     print("[Flask server.py] POST path /createuser")
@@ -65,9 +67,28 @@ def create_user():
         u["password"] = bcrypt.generate_password_hash(u["password"])
     print(userList)
     res = datum.insert_many(userList)
+    client.close()
     if res:
         return {"res": "success"}
     return {"res": "fail"}
+
+# handling password changing request from react
+# param: object of groupNo, oldPassword, and newPassword
+# return: true on successful update and false on failed update
+@app.route('/changepassword', methods=['POST'])
+def change_password():
+    print("[Flask server.py] POST path /changepassword")
+    client = MongoClient(MONGODB_URI)
+    db = client[MONGODB_DB_NAME]
+    datum = db[MONGODB_COLLECTION]
+    group = datum.find_one({"groupNo": request.json["groupNo"]})
+    if (group == None):
+        return {"res": False}
+    if (bcrypt.check_password_hash(group["password"], request.json["oldPassword"]) == False):
+        return {"res": False}
+    datum.find_one_and_update({"groupNo": request.json["groupNo"]}, {"$set": {"password": bcrypt.generate_password_hash(request.json["newPassword"])}})
+    client.close()
+    return {"res": True}
 
 if __name__ == "__main__":
     app.run(host="192.168.118.143", port=5000, debug=True)
